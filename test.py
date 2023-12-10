8831120ad1543a39b53046e173dd70cea9e4be57
mynor <mynormend@gmail.com>
2023-12-10 14:17:08 -0500

    Test file that isn't commented out is for the rt60

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-"""import numpy as np
+import numpy as np
 from pydub import AudioSegment
 import matplotlib.pyplot as plt
+from scipy.io import wavfile
 
+"""
 
 def plot_specific_frequencies(audio_path, frequency_range):
     # Load the audio file
@@ -49,12 +51,7 @@ audio_path = "C:/Users/Mynor/PycharmProjects/CSFinalProject/PinkPanther30.wav"
 frequency_range = (3000,12000)  # Specify the frequency range of interest
 plot_specific_frequencies(audio_path, frequency_range)"""
 
-"""import numpy as np
-import matplotlib.pyplot as plt
-from scipy.io import wavfile
-from pydub import AudioSegment
-
-
+"""
 def mp3_to_wav(input_mp3_path, output_wav_path):
     # Load the MP3 file
     audio = AudioSegment.from_file(input_mp3_path, format="mp3")
@@ -67,16 +64,16 @@ output_wav_path = "output.wav"
 
 mp3_to_wav(input_mp3_path, output_wav_path)
 
-sample_rate, data = wavfile.read(output_wav_path)
+sample_rate, data = wavfile.read('16bit1chan.wav')
 
 spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024)
 print("Shape of spectrum:", spectrum.shape)
 print("Shape of freqs:", freqs.shape)
-
+plt.close()
 
 def find_target_frequency(freqs):
     for x in freqs:
-        if x > 1001:
+        if x > 1000:
             break
     return x
 
@@ -125,12 +122,12 @@ value_of_max_less_25 = value_of_max - 25
 value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
 index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
 plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'go')
-# Assuming `spectrum` contains the spectrogram data
 
+# Assuming `spectrum` contains the spectrogram data
 # Define frequency band ranges
-low_freq_range = (0, 500)
-mid_freq_range = (500, 2000)
-high_freq_range = (2000, 8000)
+low_freq_range = (60,250)
+mid_freq_range = (250, 1000)
+high_freq_range = (1000, 10000)
 
 # Calculate the sum of power across frequency bands for each time point
 low_freq_power = np.sum(spectrum[(freqs >= low_freq_range[0]) & (freqs <= low_freq_range[1])], axis=0)
@@ -164,9 +161,9 @@ index_of_max_low_less_25 = np.where(low_freq_power >= value_of_max_low_less_25)[
 index_of_max_mid_less_25 = np.where(mid_freq_power >= value_of_max_mid_less_25)[0][0]
 index_of_max_high_less_25 = np.where(high_freq_power >= value_of_max_high_less_25)[0][0]
 
-plt.plot(t[index_of_max_low], data_in_db[index_of_max_low], 'go')
-plt.plot(t[index_of_max_mid], data_in_db[index_of_max_mid], 'go')
-plt.plot(t[index_of_max_high], data_in_db[index_of_max_high], 'go')
+plt.plot(t[index_of_max_low], data_in_db[index_of_max_low], 'bo')
+plt.plot(t[index_of_max_mid], data_in_db[index_of_max_mid], 'bo')
+plt.plot(t[index_of_max_high], data_in_db[index_of_max_high], 'bo')
 
 
 plt.plot(t[index_of_max_low_less_5], data_in_db[index_of_max_low_less_5], 'yo')
@@ -196,8 +193,117 @@ print(f"The RT60 reverb time for high frequencies is {np.round(abs(rt60_high), 2
 plt.grid()
 
 plt.show()
-
 """
 
-import scipy.fft
+"""
+def audiosegment_to_db(audiosegment):
+    # Convert the AudioSegment to a NumPy array
+    samples = np.array(audiosegment.get_array_of_samples())
 
+    # Calculate the decibel values
+    db_values = 20 * np.log10(np.abs(samples) / (np.iinfo(samples.dtype).max+1))
+
+    return db_values
+
+
+# Example usage
+input_mp3_path = 'Thursday_at_13-43.m4a'
+audio = AudioSegment.from_file(input_mp3_path, format='m4a')
+
+# Get decibel values
+db_values = audiosegment_to_db(audio)
+
+# Plotting
+samples = np.array(audio.get_array_of_samples())
+duration_in_sec = len(audio) / 1000
+time = np.linspace(0, duration_in_sec, num=len(samples))
+plt.plot(time, db_values, color='blue')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Decibel (dB)')
+plt.title('Decibel values of the AudioSegment')
+plt.grid(True)
+plt.show()"""
+
+loop = {1: 20, 2: 100, 3: 500}
+
+
+def mp3_to_wav(input_mp3_path, output_wav_path):
+    # Load the MP3 file
+    audio = AudioSegment.from_file(input_mp3_path, format="mp3")
+    audio = audio.set_channels(1)
+    audio.export(output_wav_path, format="wav")
+
+
+input_mp3_path = 'mgscodec.mp3'
+output_wav_path = "output.wav"
+
+mp3_to_wav(input_mp3_path, output_wav_path)
+
+sample_rate, data = wavfile.read('PinkPanther30.wav')
+print(wavfile.read('PinkPanther30.wav'))
+spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024)
+plt.close()
+
+
+def find_target_frequency(freqs, target_freq):
+    for x in freqs:
+        if x > target_freq:
+            break
+    return x
+
+
+def frequency_check(target_freq):
+    global target_frequency
+    target_frequency = find_target_frequency(freqs, target_freq)
+    index_of_frequency = np.where(freqs == target_frequency)[0][0]
+
+    data_for_frequency = spectrum[index_of_frequency]
+
+    data_in_db_fun = 10 * np.log10(data_for_frequency + 1)
+    return data_in_db_fun
+
+
+data_in_db = frequency_check(1000)
+
+plt.figure(2)
+
+plt.plot(t, data_in_db, linewidth=1, alpha=.7, color='#004bc6')
+
+plt.xlabel('Time (s)')
+
+plt.ylabel('Power (db)')
+
+index_of_max = np.argmax(data_in_db)
+print(f'value of max = {t[index_of_max]}')
+value_of_max = data_in_db[index_of_max]
+print(f'value of max = {value_of_max}')
+plt.plot(t[index_of_max], data_in_db[index_of_max], 'go')
+
+sliced_array = data_in_db[index_of_max:]
+print(f'value of max = {sliced_array}')
+value_of_max_less_5 = value_of_max - 5
+print(f'value of max = {value_of_max_less_5}')
+
+def find_nearest_value(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+
+value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
+plt.plot(t[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
+
+value_of_max_less_25 = value_of_max - 25
+value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
+index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
+plt.plot(t[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
+
+rt20 = (t[index_of_max_less_5] - t[index_of_max_less_25])
+rt60 = 3 * rt20
+
+plt.grid()
+
+plt.show()
+
+print(f'The RT60 reverb time at freq {int(target_frequency)} Hz is {np.round(abs(rt60), 3)} seconds')
